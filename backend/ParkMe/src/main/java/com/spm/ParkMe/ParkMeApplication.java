8bd752779f89b9d1024cd7cb54f3c9511da28e9e
Manuel Cretone <38427725+manuel-cretone@users.noreply.github.com>
2020-11-04 15:05:56 +0100

    setup spring backend

---
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/ParkMeApplication.java
@@ -0,0 +1,13 @@
+package com.spm.ParkMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkMeApplication.class, args);
+	}
+
+}
---
+++ backend/ParkMe/src/test/java/com/spm/ParkMe/ParkMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spm.ParkMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
