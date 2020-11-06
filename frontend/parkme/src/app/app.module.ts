332d84891400e2dd7fdd6429396763de0fdcfd2b
fala-flash <andrea0falaschini@gmail.com>
2020-11-06 12:49:54 +0100

    driver registration form added

--- frontend/parkme/src/app/app.module.ts
+++ frontend/parkme/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { DriverRegistrationComponent } from './pages/driver-registration/driver-registration.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, DriverRegistrationComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
