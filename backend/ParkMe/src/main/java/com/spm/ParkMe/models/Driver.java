83c1d5129eff8deda5af7c579da84de750566580
Manuel Cretone <38427725+manuel-cretone@users.noreply.github.com>
2020-11-08 10:22:29 +0100

    Updated Driver class and first try with tests

--- backend/ParkMe/src/main/java/com/spm/ParkMe/models/Driver.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/models/Driver.java
@@ -4,21 +4,32 @@
 package com.spm.ParkMe.models;
 
 import org.springframework.data.annotation.Id;
-/**
- * @author manuel
- *
- */
+
+
+
+
 public class Driver {
 
 	@Id private String id;
 	
 	private String firstName;
 	private String lastName;
-	private String username;
 	private String ssn;
-	private String licensePlace;
+	private String email;
+	private Number phone;
+	private String plate;
 	private String vehicleType;
-	private String hashedPassword;
+	private String password;
+	private boolean isLoading;
+	
+	
+	/*-------Constructor------*/
+	public Driver(String firstName) {
+		this.firstName = firstName;
+
+	}
+	
+	
 	
 	/*------- ACCESSORY METHODS ---------*/
 	public String getFirstName() {
@@ -33,35 +44,80 @@ public class Driver {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+	
 	public String getSsn() {
 		return ssn;
 	}
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
 	}
-	public String getLicensePlace() {
-		return licensePlace;
-	}
-	public void setLicensePlace(String licensePlace) {
-		this.licensePlace = licensePlace;
-	}
+	
 	public String getVehicleType() {
 		return vehicleType;
 	}
 	public void setVehicleType(String vehicleType) {
 		this.vehicleType = vehicleType;
 	}
-	public String getHashedPassword() {
-		return hashedPassword;
+	
+
+
+
+	public String getEmail() {
+		return email;
 	}
-	public void setHashedPassword(String hashedPassword) {
-		this.hashedPassword = hashedPassword;
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getPlate() {
+		return plate;
+	}
+
+
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+
+
+	public Number getPhone() {
+		return phone;
+	}
+
+
+
+	public void setPhone(Number phone) {
+		this.phone = phone;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public boolean isLoading() {
+		return isLoading;
+	}
+
+
+
+	public void setLoading(boolean isLoading) {
+		this.isLoading = isLoading;
 	}
 	
 	
