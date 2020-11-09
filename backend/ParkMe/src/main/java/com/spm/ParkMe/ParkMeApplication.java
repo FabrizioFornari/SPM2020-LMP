26692c8951e661da015767701f79ffc62531c887
fala-flash <andrea0falaschini@gmail.com>
2020-11-09 18:29:52 +0100

    deleted unused test methods in main class

--- backend/ParkMe/src/main/java/com/spm/ParkMe/ParkMeApplication.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/ParkMeApplication.java
@@ -1,51 +1,22 @@
 package com.spm.ParkMe;
 
-import org.springframework.boot.CommandLineRunner;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.spm.ParkMe.encryption.Encryptor;
-import com.spm.ParkMe.enums.Roles;
-import com.spm.ParkMe.models.User;
-import com.spm.ParkMe.repositories.UserRepository;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 
 @SpringBootApplication
-public class ParkMeApplication implements CommandLineRunner {
+public class ParkMeApplication {
 
-	@Autowired
-	private UserRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkMeApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		this.populateDB();
-	}
-
-<<<<<<< HEAD
-	private void populateDB() {
-=======
-	    
-	    for (User user : repository.findAll()) {
-	      System.out.println(user.getUserName());
-	    }
-	    System.out.println();
->>>>>>> 9932bc9c8bd2e5f4faf91106d0ebde9c0456433f
-
-		User[] users = null;
-		users = new User[] { new User("Cret", Encryptor.encryptPassword("Cret"), Roles.VIGILANT),
-				new User("Rocche", Encryptor.encryptPassword("Rocche"), Roles.DRIVER),
-				new User("Flash", Encryptor.encryptPassword("Flash"), Roles.PARKING_MANAGER),
-				new User("Fusaro", Encryptor.encryptPassword("Fusaro"), Roles.ADMIN), };
-		repository.deleteAll();
-		for (User user : users) {
-			repository.save(user);
-		}
-	}
+	
 }
