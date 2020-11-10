c820fb549e5b9154ecb5505aa9f4c61c432ce6c1
fala-flash <andrea0falaschini@gmail.com>
2020-11-10 17:05:08 +0100

    driver register form validation done

--- frontend/parkme/src/app/pages/driver-registration/driver-registration.component.ts
+++ frontend/parkme/src/app/pages/driver-registration/driver-registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DriverAuthService } from 'src/app/services/driver-auth.service';
 import { DriverRegistrationValidationService } from 'src/app/services/driver-registration-validation.service';
 import { ToastrService } from "ngx-toastr";
+
 @Component({
   selector: 'app-driver-registration',
   templateUrl: './driver-registration.component.html',
@@ -12,7 +13,7 @@ export class DriverRegistrationComponent implements OnInit {
   surname: string;
   ssn: string;
   email: string;
-  phone: number;
+  phone: string;
   plate: string;
   vehicleType: Array<String> = [
     '2 Wheels Vehicle',
@@ -41,7 +42,6 @@ export class DriverRegistrationComponent implements OnInit {
   ngOnInit(): void {}
 
   onLoginSubmit() {
-    this.isLoading = true;
 
     const user = {
       firstName: this.name,
@@ -54,49 +54,49 @@ export class DriverRegistrationComponent implements OnInit {
       password: this.password,
     };
 
-    if (this.registerValidate.validateName) {
+    if (this.registerValidate.validateName(user.firstName)){
       this.nameError = false;
     } else {
       this.nameError = true;
     }
 
-    if (this.registerValidate.validateSurname) {
+    if (this.registerValidate.validateSurname(user.lastName)) {
       this.surnameError = false;
     } else {
       this.surnameError = true;
     }
 
-    if (this.registerValidate.validateSSN) {
+    if (this.registerValidate.validateSSN(user.ssn)) {
       this.ssnError = false;
     } else {
       this.ssnError = true;
     }
 
-    if (this.registerValidate.validateEmail) {
+    if (this.registerValidate.validateEmail(user.email)) {
       this.emailError = false;
     } else {
       this.emailError = true;
     }
 
-    if (this.registerValidate.validatePhone) {
+    if (this.registerValidate.validatePhone(user.phone)) {
       this.phoneError = false;
     } else {
       this.phoneError = true;
     }
 
-    if (this.registerValidate.validatePlate) {
+    if (this.registerValidate.validatePlate(user.plate)) {
       this.plateError = false;
     } else {
       this.plateError = true;
     }
 
-    if (this.registerValidate.validateVehicle) {
+    if (this.registerValidate.validateVehicle(user.vehicleType)) {
       this.vehicleError = false;
     } else {
       this.vehicleError = true;
     }
 
-    if (this.registerValidate.validatePassword) {
+    if (this.registerValidate.validatePassword(user.password)) {
       this.passwordError = false;
     } else {
       this.passwordError = true;
@@ -112,14 +112,24 @@ export class DriverRegistrationComponent implements OnInit {
       !this.vehicleError &&
       !this.passwordError
     ) {
+      this.isLoading = true;
       this.driverAuthService.register(user).subscribe(
-        (data) => {
-          console.log(data);
-          this.toastrService.success('Successfully Logged In');
+        () => {
+          this.toastrService.success('Successfully Registered');
           this.isLoading = false;
         },
         (error) => {
-          console.log(error);
+          if (error.status == 401) {
+            this.toastrService.warning('Bad Credentials');
+          } else if (error.status == 403){
+            this.toastrService.warning('Forbidden');
+          } else if (error.status == 500){
+            this.toastrService.warning('Server Error');
+          } else if (error.status == 226){
+            this.toastrService.warning('Email Already in Use');
+          } else {
+            this.toastrService.warning('Unknown Error');
+          }
           this.isLoading = false;
         }
       );
--- frontend/parkme/src/app/services/driver-registration-validation.service.ts
+++ frontend/parkme/src/app/services/driver-registration-validation.service.ts
@@ -1,31 +1,44 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DriverRegistrationValidationService {
+  constructor() {}
 
-  constructor() { }
+  validateName(name: string) {
+    return name && name.trim() !== '';
+  }
 
-  validateName(name: string){}
+  validateSurname(surname: string) {
+    return surname && surname.trim() !== '';
+  }
 
-  validateSurname(surname: string){}
-
-  validateSSN(ssn: string){}
+  validateSSN(ssn: string) {
+    const re = /^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i;
+    return re.test(String(ssn).toLocaleLowerCase());
+  }
 
   validateEmail(email: string) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
 
-  validatePhone(phone: number){}
+  validatePhone(phone: string) {
+    const re = /^((00|\+)39[\. ]??)??3\d{2}[\. ]??\d{6,7}$/;
+    return re.test(String(phone));
+  }
 
-  validatePlate(plate: string){}
+  validatePlate(plate: string) {
+    const re = /[A-Za-z]{2}[0-9]{3}[A-Za-z]{2}/g;
+    return re.test(String(plate).toLocaleLowerCase());
+  }
 
-
-  validateVehicle(vehicle: string){}
+  validateVehicle(vehicle: string) {
+    return vehicle && vehicle.trim() !== '';
+  }
 
   validatePassword(password: string) {
-    return (password && password.trim()!== "");
+    return password && password.trim() !== '';
   }
 }
