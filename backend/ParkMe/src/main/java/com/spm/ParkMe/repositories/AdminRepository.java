73df931c71f25c7f4ba0cc8df7c6b2b94b5a6615
Manuel Cretone <38427725+manuel-cretone@users.noreply.github.com>
2020-11-10 15:01:00 +0100

    Added Admin Repository interface for interacting with the database

---
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/repositories/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.spm.ParkMe.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+import com.spm.ParkMe.models.ParkingManager;
+
+public interface AdminRepository extends MongoRepository<ParkingManager, ParkingManager> {
+	
+	
+}
