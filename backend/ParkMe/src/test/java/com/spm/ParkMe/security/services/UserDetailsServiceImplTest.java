69d8ff1495f1cb5462bc67c5ac0b66486793fdd5
Rocche <giacomorocchetti97@gmail.com>
2020-11-11 16:34:58 +0100

    UserDetailsService test: setup and throw error when user does not exist

---
+++ backend/ParkMe/src/test/java/com/spm/ParkMe/security/services/UserDetailsServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.spm.ParkMe.security.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.spm.ParkMe.enums.Roles;
+import com.spm.ParkMe.models.User;
+import com.spm.ParkMe.repositories.UserRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+class UserDetailsServiceImplTest {
+	
+	@InjectMocks
+	private UserDetailsServiceImpl userService;
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@Rule
+	public MockitoRule rule = MockitoJUnit.rule();
+	
+
+	@Test
+	void throwErrorWhenUserDoesNotExist() {
+		Mockito.when(userRepository.findByUsername(Mockito.anyString())).thenReturn(Optional.empty());
+		
+		assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername("a@a"));
+	}
+
+}
