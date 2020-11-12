a8e6821461555649fee3b2d7c95db80323692acf
Rocche <giacomorocchetti97@gmail.com>
2020-11-12 16:27:28 +0100

    Refactored Driver class

--- backend/ParkMe/src/main/java/com/spm/ParkMe/models/Driver.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/models/Driver.java
@@ -3,132 +3,56 @@
  */
 package com.spm.ParkMe.models;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.data.annotation.Id;
 
+import com.spm.ParkMe.enums.Roles;
 
 
 
-public class Driver {
 
-	@Id private String id;
+public class Driver extends User{
 	
-	private String firstName;
-	private String lastName;
-	private String ssn;
-	private String email;
-	private Number phone;
+	@NotNull(message="Plate must not be null")
+	@NotEmpty(message = "Plate must not be empty")
+	@Pattern(regexp="[A-Za-z]{2}[0-9]{3}[A-Za-z]{2}",message="Invalid Plate format")
 	private String plate;
+	
+	@NotNull(message="Vehicle type must not be null")
+	@NotEmpty(message = "Vehicle type must not be empty")
 	private String vehicleType;
-	private String password;
-	private boolean isLoading;
 	
-	
-	/*-------Constructor------*/
-	public Driver() {
-		
-
+	public Driver(String username, String firstName, String lastName, String ssn, String phone, String email, String password, String plate, String vehicleType) {
+		super(username, firstName, lastName, ssn, phone, email, password, Roles.ROLE_DRIVER);
+		this.setPlate(plate);
+		this.setVehicleType(vehicleType);
 	}
 	
-	
-	
-	/*------- ACCESSORY METHODS ---------*/
-	public String getFirstName() {
-		return firstName;
+	public String getPlate() {
+		return plate;
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public String getSsn() {
-		return ssn;
-	}
-	public void setSsn(String ssn) {
-		this.ssn = ssn;
+	public void setPlate(String plate) {
+		if(plate != null && plate != "" && 
+				plate.matches("[A-Za-z]{2}[0-9]{3}[A-Za-z]{2}")) {
+			this.plate = plate;
+		}
+		else {
+			throw new IllegalArgumentException("Plate is invalid");
+		}
 	}
 	
 	public String getVehicleType() {
 		return vehicleType;
 	}
 	public void setVehicleType(String vehicleType) {
-		this.vehicleType = vehicleType;
+		if(vehicleType != null && vehicleType != "") {
+			this.vehicleType = vehicleType.trim();
+		}
+		else {
+			throw new IllegalArgumentException("Vehicle type is invalid");
+		}
 	}
-	
-
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-
-	public String getPlate() {
-		return plate;
-	}
-
-
-
-	public void setPlate(String plate) {
-		this.plate = plate;
-	}
-
-
-
-	public Number getPhone() {
-		return phone;
-	}
-
-
-
-	public void setPhone(Number phone) {
-		this.phone = phone;
-	}
-
-
-
-	public String getPassword() {
-		return password;
-	}
-
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-
-	public boolean isLoading() {
-		return isLoading;
-	}
-
-
-
-	public void setLoading(boolean isLoading) {
-		this.isLoading = isLoading;
-	}
-	
-	public boolean isValid() {
-		return (this.getFirstName() != ""
-				&& this.getLastName() != "" 
-				&& this.getEmail() != ""
-				&& this.getPassword() != ""
-				&& this.getPhone() != null
-				&& this.getPlate() != ""
-				&& this.getSsn() != ""
-				&& this.getVehicleType() != "");
-	}
-	
 }
