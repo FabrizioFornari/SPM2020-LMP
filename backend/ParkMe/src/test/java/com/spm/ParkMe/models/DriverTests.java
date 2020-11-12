7be5fd3ab6707cfd80d6a56136e97769b0996797
Rocche <giacomorocchetti97@gmail.com>
2020-11-12 11:22:37 +0100

    Driver class test: creating valid driver

--- backend/ParkMe/src/test/java/com/spm/ParkMe/models/DriverTests.java
+++ backend/ParkMe/src/test/java/com/spm/ParkMe/models/DriverTests.java
@@ -1,12 +1,38 @@
 package com.spm.ParkMe.models;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class DriverTests {
 
 	
-	Driver driver =new Driver("Manuel");
+	private String validSSN;
+	private String validEmail;
+	private String validPhone;
+	private String validPlate;
+	
+	@BeforeEach
+	public void setUp() {
+		validSSN = "ZZZZZZ10A01A000Z";
+		validEmail = "email@park.it";
+		validPhone = "+39 3333333333";
+		validPlate = "AA000AA";
+	}
+	
 	@Test
-	void GetFirstName() {
-		assert(driver.getFirstName() == "Manuel");
+	public void createValidDriver() {
+		Driver driver = new Driver("firstname", "lastname", validSSN, validEmail, validPhone, validPlate, "car", "password");
+		assertEquals(driver.getClass(), Driver.class);
+		assertEquals(driver.getFirstName(), "firstname");
+		assertEquals(driver.getLastName(), "lastname");
+		assertEquals(driver.getSsn(), validSSN);
+		assertEquals(driver.getEmail(), validEmail);
+		assertEquals(driver.getPhone(), validPhone);
+		assertEquals(driver.getPlate(), validPlate);
+		assertEquals(driver.getVehicleType(), "car");
+		assertEquals(driver.getPassword(), "password");
 	}
 }
