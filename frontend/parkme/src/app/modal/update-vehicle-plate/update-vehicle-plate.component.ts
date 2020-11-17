cadd47783e2ace19007de87668b28285b4bb37cf
fala-flash <andrea0falaschini@gmail.com>
2020-11-17 15:56:40 +0100

    connected validator with update plate/vehicle modal

--- frontend/parkme/src/app/modal/update-vehicle-plate/update-vehicle-plate.component.ts
+++ frontend/parkme/src/app/modal/update-vehicle-plate/update-vehicle-plate.component.ts
@@ -1,32 +1,66 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { AccountManagementValidatorService } from 'src/app/services/account-management-validator.service';
 
 @Component({
   selector: 'app-update-vehicle-plate',
   templateUrl: './update-vehicle-plate.component.html',
-  styleUrls: ['./update-vehicle-plate.component.css']
+  styleUrls: ['./update-vehicle-plate.component.css'],
 })
 export class UpdateVehiclePlateComponent implements OnInit {
-
   @Input() PLATE: string;
   @Input() VEHICLE: string;
-  newPlate: string;
+
   vehicleType: Array<String> = [
     '2 Wheels Vehicle',
     '4 Wheels Standard Vehicle',
     '4 Wheels Big Vehicle',
   ];
-  newVehicle: string;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  newPlate: string = "";
+  newVehicle: string = "";
 
-  ngOnInit(): void {
+  newPlateError: boolean = false;
+  newVehicleError: boolean = false;
+
+  isLoading: boolean = false;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private toastrService: ToastrService,
+    private acManVal: AccountManagementValidatorService
+  ) {}
+
+  ngOnInit(): void {}
+
+  updatePlateVehicle(form: { value: { newPlate: any; newVehicle: any } }) {
+    const body = {
+      newPlate: form.value.newPlate,
+      newVehicle: form.value.newVehicle,
+    };
+
+    if (this.acManVal.validatePlate(body.newPlate)) {
+      this.newPlateError = false;
+    } else {
+      this.newPlateError = true;
+    }
+
+
+    if (this.acManVal.validateVehicle(body.newVehicle)) {
+      this.newVehicleError = false;
+    } else {
+      this.newVehicleError = true;
+    }
+
+    if (!this.newPlateError && !this.newVehicleError) {
+      this.isLoading = true;
+      this.toastrService.success('Plate/Vehicle Updated');
+      console.table(body);
+      this.activeModal.dismiss();
+      this.isLoading = false;
+    } else {
+      return null;
+    }
   }
-
-  updatePlateVehicle(form){
-    console.log(`New Plate: ${form.value.newPlate}`);
-    console.log(`New Vehicle Type: ${form.value.newVehicle}`);
-    this.activeModal.dismiss();
-  }
-
 }
