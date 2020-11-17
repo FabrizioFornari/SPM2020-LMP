bea16361425d80b0f4081291bfd626500edb4e8e
fala-flash <andrea0falaschini@gmail.com>
2020-11-17 11:55:03 +0100

    account info template and button click function

--- frontend/parkme/src/app/pages/account-info/account-info.component.ts
+++ frontend/parkme/src/app/pages/account-info/account-info.component.ts
@@ -12,4 +12,20 @@ export class AccountInfoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  changeEmail(){
+    console.log('Change Email');
+  }
+
+  changePassword(){
+    console.log('Change Password');
+  }
+
+  changePhone(){
+    console.log('Change Phone');
+  }
+
+  changeVehicle_Plate(){
+    console.log('Change Vehicle/Plate');
+  }
+
 }
