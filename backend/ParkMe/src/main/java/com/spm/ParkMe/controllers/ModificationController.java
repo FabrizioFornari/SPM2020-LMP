616f8edc05f2d99ae7c5780860b5e478114125bc
Rocche <giacomorocchetti97@gmail.com>
2020-11-17 09:57:20 +0100

    Added API call for changing password

--- backend/ParkMe/src/main/java/com/spm/ParkMe/controllers/ModificationController.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/controllers/ModificationController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.spm.ParkMe.models.User;
 import com.spm.ParkMe.models.requestBody.ChangeMailInfo;
+import com.spm.ParkMe.models.requestBody.ChangePasswordInfo;
 import com.spm.ParkMe.models.requestBody.Credentials;
 import com.spm.ParkMe.repositories.UserRepository;
 
@@ -29,18 +31,31 @@ public class ModificationController {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	@PostMapping("/email")
 	public ResponseEntity<User> modifyEmail(Authentication authentication, @Valid @RequestBody ChangeMailInfo mailInfo) {
 		String authenticatedUsername = authentication.getName();
 		if(authenticatedUsername.equals(mailInfo.getCurrentEmail())) {
 			User user = repository.findByUsername(mailInfo.getCurrentEmail()).orElseThrow(() -> new UsernameNotFoundException("The provided current email is wrong."));
-			//delete user from db
-			repository.delete(user);
 			user.setUsername(mailInfo.getNewEmail());
 			user.setEmail(mailInfo.getNewEmail());
 			repository.save(user);
 			return ResponseEntity.ok(user);
 		}
 		return new ResponseEntity<User>(HttpStatus.UNAUTHORIZED);
+	}
+	
+	@PostMapping("/password")
+	public ResponseEntity<?> modifyPassword(Authentication authentication, @Valid @RequestBody ChangePasswordInfo passwordInfo){
+		String authenticatedUsername = authentication.getName();
+		User user = repository.findByUsername(authenticatedUsername).orElseThrow(() -> new UsernameNotFoundException("Cannot find user with token's username."));
+		if(encoder.encode(passwordInfo.getCurrentPassword()) == user.getPassword()) {
+			user.setPassword(encoder.encode(passwordInfo.getNewPassword()));
+			repository.save(user);
+			return ResponseEntity.ok(user);
+		}
+		return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
 	}
 }
