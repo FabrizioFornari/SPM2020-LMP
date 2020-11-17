4630a96da6f91e6e128d32d1a2e1e6065cb76f31
fala-flash <andrea0falaschini@gmail.com>
2020-11-17 15:03:57 +0100

    validation service connected with update email modal

--- frontend/parkme/src/app/modal/update-email/update-email.component.ts
+++ frontend/parkme/src/app/modal/update-email/update-email.component.ts
@@ -1,24 +1,48 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { AccountManagementValidatorService } from 'src/app/services/account-management-validator.service';
 
 @Component({
   selector: 'app-update-email',
   templateUrl: './update-email.component.html',
-  styleUrls: ['./update-email.component.css']
+  styleUrls: ['./update-email.component.css'],
 })
 export class UpdateEmailComponent implements OnInit {
-
   @Input() EMAIL: string;
   newEmail: string;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  emailError: boolean = false;
 
-  ngOnInit(): void {
+  isLoading: boolean = false;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private toastrService: ToastrService,
+    private accManValid: AccountManagementValidatorService
+  ) {}
+
+  ngOnInit(): void {}
+
+  updateEmail(form: { value: { newEmail: string } }) {
+    const body = {
+      email: form.value.newEmail,
+    };
+
+    if (this.accManValid.validateEmail(body.email)) {
+      this.emailError = false;
+    } else {
+      this.emailError = true;
+    }
+
+    if (!this.emailError) {
+      this.isLoading = true;
+      this.toastrService.success('Email Updated');
+      this.isLoading = false;
+      console.log(`New Email: ${form.value.newEmail}`);
+      this.activeModal.dismiss();
+    } else {
+      return null;
+    }
   }
-
-  updateEmail(form: { value: { newEmail: string; }; }){
-    console.log(`New Email: ${form.value.newEmail}`);
-    this.activeModal.dismiss();
-  }
-
 }
