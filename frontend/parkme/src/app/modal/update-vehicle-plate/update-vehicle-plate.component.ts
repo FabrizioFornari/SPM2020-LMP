24baf7d772829d49bb1e0d75cbb5677f2c35388b
fala-flash <andrea0falaschini@gmail.com>
2020-11-17 17:41:56 +0100

    linked plate/vehicle  service to server api call

--- frontend/parkme/src/app/modal/update-vehicle-plate/update-vehicle-plate.component.ts
+++ frontend/parkme/src/app/modal/update-vehicle-plate/update-vehicle-plate.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { AccountManagementValidatorService } from 'src/app/services/account-management-validator.service';
+import { AccountManagementService } from 'src/app/services/account-management.service';
 
 @Component({
   selector: 'app-update-vehicle-plate',
@@ -29,12 +31,14 @@ export class UpdateVehiclePlateComponent implements OnInit {
   constructor(
     public activeModal: NgbActiveModal,
     private toastrService: ToastrService,
-    private acManVal: AccountManagementValidatorService
+    private acManVal: AccountManagementValidatorService,
+    private accManSer: AccountManagementService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
 
-  updatePlateVehicle(form: { value: { newPlate: any; newVehicle: any } }) {
+  updatePlateVehicle(form: { value: { newPlate: string; newVehicle: string } }) {
     const body = {
       newPlate: form.value.newPlate,
       newVehicle: form.value.newVehicle,
@@ -55,10 +59,36 @@ export class UpdateVehiclePlateComponent implements OnInit {
 
     if (!this.newPlateError && !this.newVehicleError) {
       this.isLoading = true;
-      this.toastrService.success('Plate/Vehicle Updated');
-      console.table(body);
-      this.activeModal.dismiss();
-      this.isLoading = false;
+
+      const body2 = {
+        currentPlate: this.PLATE,
+        newPlate: body.newPlate ,
+        currentVehicleType: this.VEHICLE,
+        newVehicleType: body.newVehicle
+      }
+      this.accManSer.updateVechiclePlate(body2).subscribe(
+        () => {
+          this.toastrService.success('Successfully Updated Plate/Vehicle');
+          this.isLoading = false;
+          this.activeModal.dismiss();
+          localStorage.clear();
+          this.router.navigate(["/login"]);
+        },
+        (error) => {
+          if (error.status == 401) {
+            this.toastrService.warning('Bad Credentials');
+          } else if (error.status == 403){
+            this.toastrService.warning('Forbidden');
+          } else if (error.status == 500){
+            this.toastrService.warning('Server Error');
+          } else if (error.status == 226){
+            this.toastrService.warning('Email Already in Use');
+          } else {
+            this.toastrService.warning('Unknown Error');
+          }
+          this.isLoading = false;
+        }
+      );
     } else {
       return null;
     }
