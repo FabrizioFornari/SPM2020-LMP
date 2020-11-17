454b05857eafa5bc1ecdb8beaf1e00f42f45286a
fala-flash <andrea0falaschini@gmail.com>
2020-11-17 13:17:16 +0100

    html template for update vehicle/plate modal

--- frontend/parkme/src/app/modal/update-vehicle-plate/update-vehicle-plate.component.ts
+++ frontend/parkme/src/app/modal/update-vehicle-plate/update-vehicle-plate.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-update-vehicle-plate',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateVehiclePlateComponent implements OnInit {
 
-  constructor() { }
+  @Input() PLATE: string;
+  @Input() VEHICLE: string;
+  newPlate: string;
+  vehicleType: Array<String> = [
+    '2 Wheels Vehicle',
+    '4 Wheels Standard Vehicle',
+    '4 Wheels Big Vehicle',
+  ];
+  newVehicle: string;
+
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+  }
+
+  updatePlateVehicle(form){
+    console.log(`New Plate: ${form.value.newPlate}`);
+    console.log(`New Vehicle Type: ${form.value.newVehicle}`);
+    this.activeModal.dismiss();
   }
 
 }
