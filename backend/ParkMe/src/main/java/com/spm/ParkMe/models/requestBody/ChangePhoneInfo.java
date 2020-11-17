b72376332f0dcf71942e1108c7e986dc1269d71c
Manuel Cretone <38427725+manuel-cretone@users.noreply.github.com>
2020-11-17 09:09:23 +0100

    Created ChangePhoneInfo class for representing the body of API request for modifying  the phone number

---
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/models/requestBody/ChangePhoneInfo.java
@@ -0,0 +1,58 @@
+package com.spm.ParkMe.models.requestBody;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import com.spm.ParkMe.constants.RegexConstants;
+
+
+public class ChangePhoneInfo {
+
+	@NotNull(message="Current phone must not be null")
+	@NotEmpty(message = "Current phone must not be empty")
+	@Pattern(regexp=RegexConstants.PHONE_REGEX,message="Invalid Phone format")
+	private String currentPhone;
+	
+	@NotNull(message="New Phone must not be null")
+	@NotEmpty(message = "New Phone must not be empty")
+	@Pattern(regexp=RegexConstants.PHONE_REGEX,message="Invalid Phone format")
+	private String newPhone;
+	
+	public ChangePhoneInfo() {
+		
+	}
+	
+	public ChangePhoneInfo(String currentPhone, String newPhone) {
+		
+		this.setCurrentPhone(currentPhone);
+		this.setNewEmail(newPhone);
+	}
+	
+	public String getCurrentEmail() {
+		return currentPhone;
+	}
+	public void setCurrentPhone(String phone) {
+		if(phone != null && phone != "" && 
+				phone.matches(RegexConstants.PHONE_REGEX)) {
+			this.currentPhone = phone;
+		}
+		else {
+			throw new IllegalArgumentException("Phone is invalid");
+		}
+	}
+	
+	public String getNewEmail() {
+		return newPhone;
+	}
+	public void setNewEmail(String phone) {
+		if(phone != null && phone != "" && 
+				phone.matches(RegexConstants.PHONE_REGEX)) {
+			this.newPhone = phone;
+		}
+		else {
+			throw new IllegalArgumentException("Phone is invalid");
+		}
+	}
+	
+	
+}
