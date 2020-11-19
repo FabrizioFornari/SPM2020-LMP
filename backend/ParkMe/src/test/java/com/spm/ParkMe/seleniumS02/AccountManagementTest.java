75b2de468a48291a4d361095724168499337dbf6
fala-flash <andrea0falaschini@gmail.com>
2020-11-19 19:46:20 +0100

    selenium test for user change phone and user change plate/vehicle

--- backend/ParkMe/src/test/java/com/spm/ParkMe/seleniumS02/AccountManagementTest.java
+++ backend/ParkMe/src/test/java/com/spm/ParkMe/seleniumS02/AccountManagementTest.java
@@ -6,19 +6,20 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.TestMethodOrder;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AccountManagementTest {
+	
 
 	private static WebDriver driver;
 
@@ -57,6 +58,7 @@ class AccountManagementTest {
 
 	
 	@Test
+	@Order(1)
 	  public void changeEmail() throws Exception {
 	    driver.get("http://localhost:4200/login");
 	    driver.findElement(By.xpath("//label")).click();
@@ -73,6 +75,7 @@ class AccountManagementTest {
 	  }
 	
 	@Test
+	@Order(2)
 	public void changePassword() throws Exception{
 		driver.get("http://localhost:4200/login");
 	    driver.findElement(By.xpath("//label")).click();
@@ -93,6 +96,42 @@ class AccountManagementTest {
 	    driver.findElement(By.name("newPasswordRepeated")).sendKeys("rocche");
 	    driver.findElement(By.id("updateButton")).click();
 	}
+	
+	@Test
+	@Order(3)
+	  public void changePhone() throws Exception {
+		driver.get("http://localhost:4200/login");
+	    driver.findElement(By.xpath("//label")).click();
+	    driver.findElement(By.id("inputEmail")).clear();
+	    driver.findElement(By.id("inputEmail")).sendKeys("rocche2@park.it");
+	    driver.findElement(By.id("inputPassword")).clear();
+	    driver.findElement(By.id("inputPassword")).sendKeys("rocche");
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+	    driver.findElement(By.xpath("(//button[@type='button'])[3]")).click();
+	    driver.findElement(By.name("newPhone")).click();
+	    driver.findElement(By.name("newPhone")).clear();
+	    driver.findElement(By.name("newPhone")).sendKeys("+39 333 3434343");
+	    driver.findElement(By.id("updateButton")).click();
+	  }
+	
+	@Test
+	@Order(4)
+	  public void changePlateAndVehicle() throws Exception {
+	    driver.get("http://localhost:4200/login");
+	    driver.findElement(By.xpath("//label")).click();
+	    driver.findElement(By.id("inputEmail")).clear();
+	    driver.findElement(By.id("inputEmail")).sendKeys("rocche2@park.it");
+	    driver.findElement(By.id("inputPassword")).clear();
+	    driver.findElement(By.id("inputPassword")).sendKeys("rocche");
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+	    driver.findElement(By.xpath("(//button[@type='button'])[4]")).click();
+	    driver.findElement(By.name("newPlate")).click();
+	    driver.findElement(By.name("newPlate")).clear();
+	    driver.findElement(By.name("newPlate")).sendKeys("AB010BA");
+	    driver.findElement(By.id("inputNewVehicle")).click();
+	    new Select(driver.findElement(By.id("inputNewVehicle"))).selectByVisibleText("4 Wheels Standard Vehicle");
+	    driver.findElement(By.id("updateButton")).click();
+	  }
 
 	
 
