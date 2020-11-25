4f76780e2ee52d7f7ea7d9de890fe92f08137632
Rocche <giacomorocchetti97@gmail.com>
2020-11-25 15:16:36 +0100

    Modified createParkingLot API call
    Added check for parking lots with same number

--- backend/ParkMe/src/main/java/com/spm/ParkMe/controllers/ParkingManagerController.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/controllers/ParkingManagerController.java
@@ -14,6 +14,8 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -27,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.spm.ParkMe.models.DriverInfo;
 import com.spm.ParkMe.models.HandicapPermitsRequest;
 import com.spm.ParkMe.models.ParkingLot;
+import com.spm.ParkMe.models.User;
 import com.spm.ParkMe.repositories.ParkingLotRepository;
 
 @RestController
@@ -41,8 +44,16 @@ public class ParkingManagerController {
 	
 	@PostMapping(path=PARKING_MANAGER_CREATE_PARKINGLOT_ENDPOINT,consumes = "application/json" )
 	@PreAuthorize("hasRole('PARKING_MANAGER')")
-	public void createParkingLot(@Valid @RequestBody ParkingLot parkingLot) throws IOException {
+	public ResponseEntity<?> createParkingLot(@Valid @RequestBody ParkingLot parkingLot) throws IOException {
+		//first get the parking lots for the street
+		List<ParkingLot> parkingLots = parkingLotRepository.findByStreet(parkingLot.getStreet());
+		//check if there is already a parking lot with specified number
+		List<ParkingLot> parkingLotsWithSameNumber = parkingLots.stream().filter(lot -> lot.getNumberOfParkingLot() == parkingLot.getNumberOfParkingLot()).collect(Collectors.toList());
+		if(!parkingLotsWithSameNumber.isEmpty()) {
+			return new ResponseEntity<ParkingLot>(HttpStatus.UNAUTHORIZED);
+		}
 		parkingLotRepository.save(parkingLot);
+		return new ResponseEntity<ParkingLot>(HttpStatus.OK);
 	}
 	
 	@DeleteMapping(path=PARKING_MANAGER_DELETE_PARKINGLOT_ENDPOINT,consumes = "application/json" )
