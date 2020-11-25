7f11d8f1e93a0004ceb2d557b6e1dc70cd695f4d
Rocche <giacomorocchetti97@gmail.com>
2020-11-25 18:24:58 +0100

    Modified coordinates

--- backend/ParkMe/src/main/java/com/spm/ParkMe/constants/ParkingLotCostants.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/constants/ParkingLotCostants.java
@@ -16,7 +16,7 @@ public class ParkingLotCostants {
 	public static final Boolean VALID_ISHANDICAPPARKINGLOT = false;
 	public static final Double VALID_PRICEPERHOURS = 0.5;
 	public static final String VALID_TYPEOFVEHICLE  = "CAR";
-	public static final Coordinates VALID_COORDINATES= new Coordinates(23.56,34.6);
+	public static final Coordinates VALID_COORDINATES= new Coordinates("23.56","34.6");
 
 	
 	public static final ParkingLot PARKINGLOT_OBJECT = new ParkingLot(VALID_STREET, VALID_NUMBROFPARKINGLOT, VALID_ISHANDICAPPARKINGLOT, VALID_PRICEPERHOURS, VALID_TYPEOFVEHICLE, VALID_COORDINATES);
