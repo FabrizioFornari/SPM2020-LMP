4d8e2f5434320678f972175fa3b99c2d8a9ec6a5
fala-flash <andrea0falaschini@gmail.com>
2020-11-30 19:09:47 +0100

    fixed map zoom level

--- frontend/parkme/src/app/pages/map/map.component.ts
+++ frontend/parkme/src/app/pages/map/map.component.ts
@@ -4,102 +4,104 @@ import { icon, latLng, Map, marker, point, polyline, tileLayer } from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
-
   // Define our base layers so we can reference them multiple times
   streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    detectRetina: true
+    detectRetina: true,
+    maxNativeZoom: 18,
+    maxZoom: 100,
   });
   wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-    detectRetina: true
+    detectRetina: true,
+    maxNativeZoom: 18,
+    maxZoom: 100,
   });
-
 
   // Marker for the parking lot at Superconti
-  superconti = marker([ 43.14382255580331, 13.069720441965472 ], {
+  superconti = marker([43.14382255580331, 13.069720441965472], {
     icon: icon({
-      iconSize: [ 25, 41 ],
-      iconAnchor: [ 13, 41 ],
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
       iconUrl: 'leaflet/marker-icon.png',
       iconRetinaUrl: 'leaflet/marker-icon-2x.png',
-      shadowUrl: 'leaflet/marker-shadow.png'
-    })
+      shadowUrl: 'leaflet/marker-shadow.png',
+    }),
   });
 
-
   // Marker for the parking lot at Polo Lodovici
-  lodovici = marker([ 43.139719268413266, 13.068769368949196 ], {
+  lodovici = marker([43.139719268413266, 13.068769368949196], {
     icon: icon({
-      iconSize: [ 25, 41 ],
-      iconAnchor: [ 13, 41 ],
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
       iconUrl: 'leaflet/marker-icon.png',
       iconRetinaUrl: 'leaflet/marker-icon-2x.png',
-      shadowUrl: 'leaflet/marker-shadow.png'
-    })
+      shadowUrl: 'leaflet/marker-shadow.png',
+    }),
   });
 
   // Marker for the parking lot at Eurospin
-  eurospin = marker([ 43.144892512182224, 13.067108097785395 ], {
+  eurospin = marker([43.144892512182224, 13.067108097785395], {
     icon: icon({
-      iconSize: [ 25, 41 ],
-      iconAnchor: [ 13, 41 ],
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
       iconUrl: 'leaflet/marker-icon.png',
       iconRetinaUrl: 'leaflet/marker-icon-2x.png',
-      shadowUrl: 'leaflet/marker-shadow.png'
-    })
+      shadowUrl: 'leaflet/marker-shadow.png',
+    }),
   });
 
   // Marker for the parking lot at Contram
-  contram = marker([ 43.142463059981274, 13.077146597785356 ], {
+  contram = marker([43.142463059981274, 13.077146597785206], {
     icon: icon({
-      iconSize: [ 25, 41 ],
-      iconAnchor: [ 13, 41 ],
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
       iconUrl: 'leaflet/marker-icon.png',
       iconRetinaUrl: 'leaflet/marker-icon-2x.png',
-      shadowUrl: 'leaflet/marker-shadow.png'
-    })
+      shadowUrl: 'leaflet/marker-shadow.png',
+    }),
   });
-
-
 
   // Layers control object with our two base layers and the three overlay layers
   layersControl = {
     baseLayers: {
       'Street Maps': this.streetMaps,
-      'Wikimedia Maps': this.wMaps
+      'Wikimedia Maps': this.wMaps,
     },
     overlays: {
-      'Superconti': this.superconti,
+      Superconti: this.superconti,
       'Polo Lodovici': this.lodovici,
-      'Eurospin': this.eurospin,
-      'Contram': this.contram
-    }
+      Eurospin: this.eurospin,
+      Contram: this.contram,
+    },
   };
-
 
   // Set the initial set of displayed layers (we could also use the leafletLayers input binding for this)
   options = {
-    layers: [ this.streetMaps, this.superconti, this.lodovici, this.eurospin, this.contram ],
+    layers: [
+      this.streetMaps,
+      this.superconti,
+      this.lodovici,
+      this.eurospin,
+      this.contram,
+    ],
     zoom: 16,
-    center: latLng([ 43.14367132147207, 13.067582838096936 ])
+    center: latLng([43.14367132147207, 13.067582838096936]),
   };
 
   //set options for popup
   popOptions = {
     closeButton: false,
-    className: "leafletPopUp"
-  }
-
+    className: 'leafletPopUp',
+  };
 
   constructor() {}
 
   ngOnInit(): void {
-    this.superconti.bindPopup("Superconti", this.popOptions);
-    this.eurospin.bindPopup("Eurospin", this.popOptions);
-    this.lodovici.bindPopup("Polo Lodovici", this.popOptions);
-    this.contram.bindPopup("Contram", this.popOptions);
+    this.superconti.bindPopup('Superconti', this.popOptions);
+    this.eurospin.bindPopup('Eurospin', this.popOptions);
+    this.lodovici.bindPopup('Polo Lodovici', this.popOptions);
+    this.contram.bindPopup('Contram', this.popOptions);
   }
-
 }
