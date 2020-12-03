8377beda60063cd1ee603c6ab2a7d814365f830d
Rocche <giacomorocchetti97@gmail.com>
2020-12-03 17:59:39 +0100

    Modified ParkingLot class
    Removed unused username field

--- backend/ParkMe/src/main/java/com/spm/ParkMe/controllers/DriverController.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/controllers/DriverController.java
@@ -80,14 +80,12 @@ public class DriverController {
 
 	@PutMapping(path = DRIVER_STATUS_PARKINGLOT_SET_STATUS_BOOKED, consumes = "application/json")
 	@PreAuthorize("hasRole('DRIVER')")
-	public ResponseEntity setStatusParkingLotAsBooked(Authentication authentication,
-			@Valid @RequestBody ParkingLot parkingLot) throws IOException {
-		if (parkingLot.getStatus() == (Status.FREE) && parkingLotRepository.findByUsername(authentication.getName()).isEmpty()) {
+	public ResponseEntity setStatusParkingLotAsBooked(@Valid @RequestBody ParkingLot parkingLot) throws IOException {
+		if (parkingLot.getStatus() == (Status.FREE)) {
 			List<ParkingLot> parks = parkingLotRepository.findByStreetAndNumberOfParkingLot(parkingLot.getStreet(),
 					parkingLot.getNumberOfParkingLot());
 			ParkingLot park = parks.get(0);
 			park.setStatus(Status.BOOKED);
-			park.setUsername(authentication.getName());
 			parkingLotRepository.save(park);
 			return new ResponseEntity(HttpStatus.OK);
 		} else {
@@ -104,7 +102,6 @@ public class DriverController {
 					parkingLot.getNumberOfParkingLot());
 			ParkingLot park = parks.get(0);
 			park.setStatus(Status.FREE);
-			park.setUsername(null);
 			parkingLotRepository.save(park);
 			return new ResponseEntity(HttpStatus.OK);
 		} else {
--- backend/ParkMe/src/main/java/com/spm/ParkMe/models/ParkingLot.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/models/ParkingLot.java
@@ -27,8 +27,6 @@ public class ParkingLot {
 	
 	private Status status;
 	
-	private String username;
-	
 	/*-------Constructor------*/
 	public ParkingLot() {
 		
@@ -42,7 +40,6 @@ public class ParkingLot {
 		this.setTypeOfVehicle(typeOfVehicle);
 		this.setCoordinates(coordinates);
 		this.setStatus(Status.FREE);
-		this.setUsername(null);
 	}
 	
 	/*------- ACCESSORY METHODS ---------*/
@@ -137,14 +134,6 @@ public class ParkingLot {
 				parkingLot.getStatus().equals( this.getStatus())&&
 				parkingLot.getTypeOfVehicle().equals(this.getTypeOfVehicle()));
 		
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
 	}
 
 }
