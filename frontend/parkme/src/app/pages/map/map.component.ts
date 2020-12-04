32a137e49ecd910a3cbdc1713895eaa2e8215709
fala-flash <andrea0falaschini@gmail.com>
2020-12-04 13:02:29 +0100

    automatic search function for maps click and marker clicks

--- frontend/parkme/src/app/pages/map/map.component.ts
+++ frontend/parkme/src/app/pages/map/map.component.ts
@@ -9,7 +9,7 @@ import {
   Layer,
   marker,
   Popup,
-  tileLayer
+  tileLayer,
 } from 'leaflet';
 import { ConfirmParkingLotComponent } from 'src/app/modal/confirm-parking-lot/confirm-parking-lot.component';
 import { ParkingLotServiceService } from 'src/app/services/parking-lot-service.service';
@@ -49,6 +49,7 @@ export class MapComponent implements OnInit {
 
   streetsLayers = [];
 
+  automatic: boolean = false;
 
   constructor(
     private parkingService: ParkingLotServiceService,
@@ -57,7 +58,10 @@ export class MapComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    console.log(history.state.automatic);
+    if (history.state.automatic != null) {
+      this.automatic = history.state.automatic;
+    }
+
     this.streetsLayers = [];
     this.parkingService.driverGetStreetList().subscribe(
       (success) => {
@@ -80,7 +84,11 @@ export class MapComponent implements OnInit {
               }
             )
               .bindTooltip(item.street.toString())
-              .on('click', () => this.getParks(item.street));
+              .on('click', () =>
+                this.automatic
+                  ? this.automaticSearch(item.coordinates.latitude, item.coordinates.longitude)
+                  : this.getParks(item.street)
+              );
             this.streetsLayers.push(markerItem);
           }
         );
@@ -91,19 +99,30 @@ export class MapComponent implements OnInit {
     );
   }
 
+  automaticSearchFromClick(e){
+    if (this.automatic) {
+      this.automaticSearch(e.latlng.lat, e.latlng.lng);
+    } else return null;
+  }
+
+  automaticSearch(latitude, longitude) {
+    alert(`${latitude} / ${longitude}`);
+  }
+
+
   getParks(street: string | ((layer: Layer) => Content) | HTMLElement | Popup) {
     this.parkingService.driverGetParkingLots(street).subscribe(
       (success) => {
         console.log(success);
         let markers = [];
-        success.forEach(
-          (item) => {
-            if (item.status !== "DISABLED") {
-              let markerItem = this.createMarker(item).on('click', () => this.zone.run(() => this.openModalBookParking(item)));
-              markers.push(markerItem);
-            }
+        success.forEach((item) => {
+          if (item.status !== 'DISABLED') {
+            let markerItem = this.createMarker(item).on('click', () =>
+              this.zone.run(() => this.openModalBookParking(item))
+            );
+            markers.push(markerItem);
           }
-        );
+        });
         this.zone.run(() => (this.streetsLayers = markers));
       },
       (error) => {
@@ -117,8 +136,7 @@ export class MapComponent implements OnInit {
     if (
       !item.isHandicapParkingLot &&
       item.status == 'FREE' &&
-      item.pricePerHours == 0 
-      
+      item.pricePerHours == 0
     ) {
       html =
         "<div class='marker-pin-free'></div><i class='material-icons'>stop_circle</i>";
