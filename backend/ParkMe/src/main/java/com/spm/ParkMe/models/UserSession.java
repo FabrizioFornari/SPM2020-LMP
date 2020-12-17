182659852337ba59b5f72d734aadffefb5e38b39
Rocche <giacomorocchetti97@gmail.com>
2020-12-17 16:40:52 +0100

    Created UserSession repository for storing sessions into db

--- backend/ParkMe/src/main/java/com/spm/ParkMe/models/UserSession.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/models/UserSession.java
@@ -1,7 +1,12 @@
 package com.spm.ParkMe.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="userSessions")
 public class UserSession {
 
+	@Id private String id;
 	private User user;
 	private String sessionID;
 	
@@ -26,5 +31,16 @@ public class UserSession {
 
 	public void setSessionID(String sessionID) {
 		this.sessionID = sessionID;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		UserSession userSession = (UserSession) o;
+		return(userSession.getUser().equals(this.getUser()) &&
+				userSession.getSessionID().equals(this.getSessionID()));
 	}
 }
