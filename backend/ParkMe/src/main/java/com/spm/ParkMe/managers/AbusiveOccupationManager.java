f89c2c5ae9b1b91613301cf3d8df37038ef31e4e
Manuel Cretone <38427725+manuel-cretone@users.noreply.github.com>
2020-12-28 18:41:27 +0100

    Added get Status parking lot for getting the status of parking Lot

--- backend/ParkMe/src/main/java/com/spm/ParkMe/managers/AbusiveOccupationManager.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/managers/AbusiveOccupationManager.java
@@ -1,8 +1,13 @@
 package com.spm.ParkMe.managers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.spm.ParkMe.enums.Status;
+import com.spm.ParkMe.models.ParkingLot;
 import com.spm.ParkMe.notifications.NotificationDispatcher;
+import com.spm.ParkMe.repositories.ParkingLotRepository;
 
 public class AbusiveOccupationManager {
 
@@ -10,9 +15,19 @@ public class AbusiveOccupationManager {
 	@Autowired
 	NotificationDispatcher notificationDispatcher;
 	
+	@Autowired
+	ParkingLotRepository parkingLotRepository;
 	
 	//--------- CONSTRUCTOR --------- //
 	public AbusiveOccupationManager() {
 		
+	}
+	
+	
+	public Status getStatusParkingLot(String street, int numberOfParkingLot)
+	{
+		List<ParkingLot> parkingLots= parkingLotRepository.findByStreetAndNumberOfParkingLot(street, numberOfParkingLot);
+		ParkingLot parkingLot = parkingLots.get(0);
+		return parkingLot.getStatus();
 	}
 }
