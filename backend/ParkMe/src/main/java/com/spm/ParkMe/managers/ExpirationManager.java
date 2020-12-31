b9d07f8bd2734290652ff26f3874dbc41e47bfd0
Manuel Cretone <38427725+manuel-cretone@users.noreply.github.com>
2020-12-31 12:18:53 +0100

    Create sendNotificationToDriverBeforeTicketExpiring method
    Create sendNotificationToDriverBeforeTicketExpiring method in expiringManager class for notifying to driver the expiration of parking lot

--- backend/ParkMe/src/main/java/com/spm/ParkMe/managers/ExpirationManager.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/managers/ExpirationManager.java
@@ -1,11 +1,23 @@
 package com.spm.ParkMe.managers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.spm.ParkMe.enums.CategoryNotification;
+import com.spm.ParkMe.enums.SensorState;
+import com.spm.ParkMe.enums.Status;
+import com.spm.ParkMe.models.Notification;
+import com.spm.ParkMe.models.ParkingLot;
+import com.spm.ParkMe.models.ParkingLotBooking;
+import com.spm.ParkMe.models.ParkingLotTicket;
 import com.spm.ParkMe.notifications.NotificationDispatcher;
 import com.spm.ParkMe.repositories.ParkingLotBookingRepository;
 import com.spm.ParkMe.repositories.ParkingLotRepository;
+import com.spm.ParkMe.repositories.ParkingLotTicketRepository;
 
+@Component
 public class ExpirationManager {
 
 	
@@ -18,11 +30,45 @@ public class ExpirationManager {
 	@Autowired
 	private ParkingLotBookingRepository parkingLotBookingRepository;
 	
+	@Autowired
+	private ParkingLotTicketRepository parkingLotTicketRepository;
 	
 	
 	//--------- CONSTRUCTOR --------- //
 		public ExpirationManager() {
 			
+		}
+		
+		
+		private Status getStatusParkingLot(String street, Integer numberOfParkingLot)
+		{
+			List<ParkingLot> parkingLots = parkingLotRepository.findByStreetAndNumberOfParkingLot(street, numberOfParkingLot);
+			if(!parkingLots.isEmpty()) {
+				ParkingLot parkingLot = parkingLots.get(0);
+				return parkingLot.getStatus();
+			}
+			else {
+				return null;
+			}
+		}
+		
+		
+		public void sendNotificationToDriverBeforeTicketExpiring(String street, Integer numberOfParkingLot, String username) {
+			
+			if(!this.getStatusParkingLot(street, numberOfParkingLot).equals(Status.FREE) ) {
+				
+				List<ParkingLotTicket> parkingLotTickets = parkingLotTicketRepository.findByUsername(username);
+				ParkingLotTicket parkingLotTicket = parkingLotTickets.get(0);
+				Notification notification= new Notification("Ticket Expiring", "The ticket is about to expire. If you want to stay longer please refresh the ticket, otherwise leave the parking lot in 5 minutes.", username, System.currentTimeMillis());
+				notification.setCategoryNotification(CategoryNotification.DRIVER_EXPIRING_TICKET);
+				notificationDispatcher.sendNotificationToUser(username, notification);
+			}
+			
+			
+			
+			
+
+
 		}
 	
 }
