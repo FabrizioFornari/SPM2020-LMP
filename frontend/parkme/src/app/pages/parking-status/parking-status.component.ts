ee1408bf0a3d3c427e3e9faa3e96399fe5b417b7
fala-flash <andrea0falaschini@gmail.com>
2021-01-05 16:05:31 +0100

    parking status page html and css done

--- frontend/parkme/src/app/pages/parking-status/parking-status.component.ts
+++ frontend/parkme/src/app/pages/parking-status/parking-status.component.ts
@@ -8,12 +8,29 @@ import { Title } from '@angular/platform-browser';
 })
 export class ParkingStatusComponent implements OnInit {
 
+  parkList = [];
+
   constructor(
     private titleService: Title
   ) { }
 
   ngOnInit(): void {
     this.titleService.setTitle('ParkMe | Parking Status');
+    this.parkList = [
+      {
+        "via": "Aldo Moro"
+      },
+      {
+        "via": "Madonna delle Carceri"
+      },
+      {
+        "via": "Garibaldi"
+      }
+    ]
+  }
+
+  seeStatus(via){
+    alert(via);
   }
 
 }
