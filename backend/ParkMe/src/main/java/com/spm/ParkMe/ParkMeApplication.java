cd079e51182f98527830094800b14b692f9e44cc
Rocche <giacomorocchetti97@gmail.com>
2021-01-21 12:40:41 +0100

    Changed main call configuration

--- backend/ParkMe/src/main/java/com/spm/ParkMe/ParkMeApplication.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/ParkMeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -68,6 +69,11 @@ public class ParkMeApplication extends SpringBootServletInitializer implements C
 	public static void main(String[] args) {
 		SpringApplication.run(ParkMeApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(ParkMeApplication.class);
+	  }
 
 
 	@Override
