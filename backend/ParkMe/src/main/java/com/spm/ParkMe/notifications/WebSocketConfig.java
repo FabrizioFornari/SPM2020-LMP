e2081cff24dc78c8cb976b6d20e3e3670b0481e0
Rocche <giacomorocchetti97@gmail.com>
2021-01-21 11:14:39 +0100

    New build with routing hash trategy

--- backend/ParkMe/src/main/java/com/spm/ParkMe/notifications/WebSocketConfig.java
+++ backend/ParkMe/src/main/java/com/spm/ParkMe/notifications/WebSocketConfig.java
@@ -12,18 +12,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/notification", "/ParkMe/notification");
-        registry.setApplicationDestinationPrefixes("/swns", "/ParkMe/swns");
+        registry.enableSimpleBroker("/notification", "/SPM2020-LMP/notification");
+        registry.setApplicationDestinationPrefixes("/swns", "/SPM2020-LMP/swns");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/notifications")
-                .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:8080/ParkMe", "http://127.0.0.1:8080/ParkMe")
+                .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:8080/SPM2020-LMP", "http://127.0.0.1:8080/SPM2020-LMP")
                 .withSockJS();
         
-        registry.addEndpoint("/ParkMe/notifications")
-        .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:8080/ParkMe", "http://127.0.0.1:8080/ParkMe")
+        registry.addEndpoint("/SPM2020-LMP/notifications")
+        .setAllowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:8080/SPM2020-LMP", "http://127.0.0.1:8080/SPM2020-LMP")
         .withSockJS();
     }
 }
