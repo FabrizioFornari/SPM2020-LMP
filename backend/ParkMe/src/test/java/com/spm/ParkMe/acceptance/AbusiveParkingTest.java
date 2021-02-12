7d6b11f65a10d1940e45b66792bbeb195165e97a
Manuel Cretone <38427725+manuel-cretone@users.noreply.github.com>
2021-02-12 10:51:02 +0100

    Disabled acceptance tests

--- backend/ParkMe/src/test/java/com/spm/ParkMe/acceptance/AbusiveParkingTest.java
+++ backend/ParkMe/src/test/java/com/spm/ParkMe/acceptance/AbusiveParkingTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -74,7 +75,7 @@ public class AbusiveParkingTest {
 	}
 	
 	
-	
+	@Disabled
 	@Test
 	@Tag("AcceptanceTest")
 	  public void abusiveParkingNotificationTest() throws Exception {
--- backend/ParkMe/src/test/java/com/spm/ParkMe/acceptance/TicketExpiringTest.java
+++ backend/ParkMe/src/test/java/com/spm/ParkMe/acceptance/TicketExpiringTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -78,6 +79,7 @@ public class TicketExpiringTest {
 	    }
 	}
 	
+	@Disabled
 	@Test
 	@Tag("AcceptanceTest")
 	 public void testUntitledTestCase() throws Exception {
